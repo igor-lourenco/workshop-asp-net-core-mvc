02e5ab035fba3763eb5180bdc9e31fbc6fe96b7c
igor-lourenco <igor.lourencosantos@gmail.com>
2021-03-07 15:29:22 -0300

    Outras entidades e segunda migration

---
+++ VendasWebMVC/Models/VendasRecorde.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMVC.Models.Enums;
+
+namespace VendasWebMVC.Models {
+    public class VendasRecorde {
+
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public double Quantia { get; set; }
+        public VendaStatus Status { get; set; }
+        public Vendedor Vendedor { get; set; }
+
+        public VendasRecorde() {
+        }
+
+        public VendasRecorde(int id, DateTime data, double quantia, VendaStatus status, Vendedor vendedor) {
+            Id = id;
+            Data = data;
+            Quantia = quantia;
+            Status = status;
+            Vendedor = vendedor;
+        }
+    }
+}
---
+++ VendasWebMVC/Models/Vendedor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VendasWebMVC.Models {
+    public class Vendedor {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public double SalarioBase { get; set; }
+        public Departamento Departamento { get; set; }
+        public ICollection<VendasRecorde> Vendas { get; set; } = new List<VendasRecorde>();
+
+        public Vendedor() {
+        }
+
+        public Vendedor(int id, string nome, string email, DateTime dataNascimento, double salarioBase, Departamento departamento) {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            DataNascimento = dataNascimento;
+            SalarioBase = salarioBase;
+            Departamento = departamento;
+        }
+
+        public void AdicionarVendas(VendasRecorde vr) {
+            Vendas.Add(vr);
+        }
+        
+        public void RemoverVendas(VendasRecorde vr) {
+            Vendas.Remove(vr);
+        }
+
+        public double TotalVendas(DateTime inicio, DateTime final) {
+            return Vendas.Where(vr => vr.Data >= inicio && vr.Data <= final).Sum(sr => sr.Quantia);
+        }
+    }
+}
