d982986296c916a22750790d9805ae7d0b377574
igor-lourenco <igor.lourencosantos@gmail.com>
2021-03-07 12:44:42 -0300

    CRUD Scaffolding

--- VendasWebMVC/Controllers/HomeController.cs
+++ VendasWebMVC/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using VendasWebMVC.Models;
 
+
 namespace VendasWebMVC.Controllers {
     public class HomeController : Controller {
         public IActionResult Index() {
