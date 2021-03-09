a5ca75fb96c86ea653e7ff4a984b75d17b83ccbc
igor-lourenco <igor.lourencosantos@gmail.com>
2021-03-09 15:04:00 -0300

    Deletar vendedor

--- VendasWebMVC/Servicos/VendedorServico.cs
+++ VendasWebMVC/Servicos/VendedorServico.cs
@@ -25,5 +25,21 @@ namespace VendasWebMVC.Servicos {
             _context.Add(obj);
             _context.SaveChanges();
         }
+
+        public Vendedor BuscarPorId(int id) {
+
+            return _context.Vendedor.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remover(int id) {
+
+            //Pega o objeto passando o id
+            var obj = _context.Vendedor.Find(id);
+            //Remove no DBSet
+            _context.Vendedor.Remove(obj);
+            //Framework deleta no banco de dados
+            _context.SaveChanges();
+
+        }
     }
 }
