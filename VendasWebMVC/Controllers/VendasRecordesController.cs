330ed7e11d79cd3167a3ffd96d4c6068dabb890d
igor-lourenco <igor.lourencosantos@gmail.com>
2021-03-16 20:38:44 -0300

    Preparando a navegacao de telas de pesquisa de vendas

---
+++ VendasWebMVC/Controllers/VendasRecordesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VendasWebMVC.Controllers
+{
+    public class VendasRecordesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaSimples() {
+            return View();
+        }
+
+        public IActionResult BuscaAgrupada() {
+            return View();
+        }
+    }
+}
